48b0e4cf9e805a97dca5322d2ece6d04d4d8d8b6
alexomantovani <alexomantovani926@gmail.com>
2024-12-13 15:01:52 -0300

    feat: add privacy_policy.py

---
+++ privacy_policy.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Título da página
+st.title("Política de Privacidade")
+
+# Descrição da Política de Privacidade
+st.markdown("""
+    **Política de Privacidade**
+
+    Este aplicativo coleta e usa informações pessoais apenas da maneira descrita nesta Política de Privacidade. A sua privacidade é importante para nós.
+
+    **Coleta de Informações:**
+    - Não coletamos informações pessoais identificáveis sem o seu consentimento.
+    - O aplicativo pode coletar dados relacionados ao uso do dispositivo (por exemplo, tipo de dispositivo, versão do sistema operacional, preferências do usuário, etc.) para melhorar a experiência do usuário.
+
+    **Uso das Informações:**
+    - Os dados coletados são utilizados exclusivamente para melhorar a funcionalidade e o desempenho do aplicativo.
+    - Nenhuma informação pessoal identificável será compartilhada com terceiros sem o seu consentimento expresso.
+
+    **Segurança:**
+    - A segurança das suas informações pessoais é importante para nós, mas lembre-se de que nenhum método de transmissão pela internet ou armazenamento eletrônico é 100% seguro. Embora nos esforcemos para usar meios comercialmente aceitáveis para proteger suas informações pessoais, não podemos garantir sua segurança absoluta.
+
+    **Alterações nesta Política de Privacidade:**
+    - Podemos atualizar nossa Política de Privacidade periodicamente. Informaremos os usuários sobre qualquer alteração, publicando a nova política nesta página.
+
+    Se você tiver alguma dúvida sobre nossa Política de Privacidade, entre em contato conosco através de oliveira.datacontrol@outlook.com.
+""")
